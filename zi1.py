6fd91ba08ad9546db6a649814db2977866289380
gsedometov <gs@sedometov.com>
2016-11-07 19:02:08 +0500

    Add files via upload

--- zi1.py
+++ zi1.py
@@ -1,20 +1,25 @@
+from argparse import ArgumentParser
 from random import choice
 from string import ascii_letters, digits
 
 ALPHABET = ascii_letters + digits
-n_letters = len(ALPHABET)
 
 def generate_password(pwd_len):
     return ''.join([choice(ALPHABET) for i in range(pwd_len)])
 
 def encrypt(pwd, bias=2):
-    idxs = ((ALPHABET.index(c) + bias) % n_letters for c in pwd)
+    idxs = ((ALPHABET.index(c) + bias) % len(ALPHABET) for c in pwd)
     return ''.join((ALPHABET[i] for i in idxs))
 
+def parse_args():
+    parser = ArgumentParser(description='Генерация паролей заданной длины.')
+    parser.add_argument('-l', type=int, help='Длина паролей.')
+    parser.add_argument('-n', type=int, help='Количество паролей.')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    n_pwds = int(input('Введите количество паролей: '))
-    pwd_len = int(input('Введите длину пароля: '))
-    pwds = [generate_password(pwd_len) for _ in range(n_pwds)]
+    args = parse_args()
+    pwds = [generate_password(args.l) for _ in range(args.n)]
     print('Сгенерированные пароли:', *pwds, sep='\n')
     with open('passwords.txt', 'w') as output:
         encrypted = (encrypt(p) for p in pwds)
